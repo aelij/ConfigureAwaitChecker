fb8102ee5c580ee07b02fdea7ecd5f180e546e7a
Eli Arbel <elarbel@microsoft.com>
2021-01-26 19:38:27 +0200

    Add publish option

--- src/Arbel.ReSharper.ConfigureAwaitPlugin/package.ps1
+++ src/Arbel.ReSharper.ConfigureAwaitPlugin/package.ps1
@@ -1,4 +1,26 @@
-dotnet pack $PSScriptRoot/Arbel.ReSharper.ConfigureAwaitPlugin.csproj -c Release
-dotnet build $PSScriptRoot/Arbel.Rider.ConfigureAwaitPlugin.csproj -c Release
-Copy-Item $PSScriptRoot/bin/Arbel.ReSharper.ConfigureAwaitPlugin/Release/*.nupkg $PSScriptRoot -Force
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]
+    $Publish
+)
+
+$ErrorActionPreference = 'Stop'
+
+function native([ScriptBlock] $ScriptBlock) {
+  & $ScriptBlock
+  if ($LastExitCode -ne 0) {
+    throw "Program exited with code $LastExitCode"
+  }
+}
+
+native { dotnet pack $PSScriptRoot/Arbel.ReSharper.ConfigureAwaitPlugin.csproj -c Release }
+native { dotnet build $PSScriptRoot/Arbel.Rider.ConfigureAwaitPlugin.csproj -c Release }
 Compress-Archive $PSScriptRoot/bin/Arbel.Rider.ConfigureAwaitPlugin/Release/net461/plugin $PSScriptRoot/plugin.zip -Force
+Copy-Item $PSScriptRoot/bin/Arbel.ReSharper.ConfigureAwaitPlugin/Release/*.nupkg $PSScriptRoot -Force
+
+if ($Publish) {
+  $token = $env:JB_TOKEN
+  native { curl -i --header "Authorization: Bearer $token" -F pluginId=10948 -F file=@$PSScriptRoot/plugin.zip -F channel=Stable 'https://plugins.jetbrains.com/plugin/uploadPlugin' }
+  Get-ChildItem $PSScriptRoot/*.nupkg | ForEach-Object { native { dotnet nuget push $_.FullName $token --source 'https://plugins.jetbrains.com/' } }
+}
